f559b82d38b6d0c18064ed44ad3d388c796654bd
njiji <noelathandi123@gmail.com>
2024-09-30 11:35:09 +0300

    arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+let num1 = 2;
+let num2 = 31;
+let multiply = num1*num2;
+console.log (multiply);
 
-// Write your code here
+let random = 7;
+
+let num3 = 4;
+let num4 = 1;
+let mod = num3/num4;
+console.log(mod);
+
+let max = Math.max(5,10,15,20);
+console.log(max);
